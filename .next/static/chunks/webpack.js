7b295fb2d8f02280a3af17a66aea83a979c4ee92
Nitya <niittss27@gmail.com>
2024-07-14 09:21:21 +0530

    Meetup pages styling added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "467dab81676bbae0"; }
+/******/ 		__webpack_require__.h = function() { return "c0d692d6248a55de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import MeetupList from '../components/meetups/MeetupList';
-import Layout from '../components/layout/layout'
+
 const DUMMY_MEETUPS = [
   {
     id: 'm1',
     title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    image:
+     'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
     address: 'Some address 5, 12345 Some City',
     description: 'This is a first meetup!'
   },
@@ -18,11 +19,7 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-  return(
-    <Layout>
-<MeetupList meetups={DUMMY_MEETUPS} />
-    </Layout>
-  ) 
+  return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage;
