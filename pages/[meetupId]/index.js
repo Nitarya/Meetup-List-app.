9114f806d2f4a4006177616dee4d53543775e129
Nitya <niittss27@gmail.com>
2024-07-15 16:06:08 +0530

    app router added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,4 @@
+import { CLIENT_STATIC_FILES_RUNTIME_WEBPACK } from 'next/dist/shared/lib/constants';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function MeetupDetails() {
@@ -10,5 +11,44 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2'
+        }
+      }
+    ]
+  }
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+console.log(meetupId)
+
+return {
+  props: {
+    meetupData: {
+      image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+    }
+  }
+}
+
+}
+
 
 export default MeetupDetails;
